aa7ec2d5fc986bd60373f757f86142c9a4b7683f
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-12-01 13:18:26 +0200

    create func get_content_from_disk

--- header/filework.h
+++ header/filework.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifndef _FILE_WORK_
 #define _FILE_WORK_
---
+++ src/filework.c
@@ -0,0 +1,46 @@
+#include "../header/filework.h"
+
+#define STR_L 200
+
+char **reset_size(char **buf, int size) {
+    if (buf == NULL) {
+        buf = malloc(sizeof(char *) * size);
+        return buf;
+    }
+    
+    buf = realloc(buf, sizeof(char *) * size);
+
+    return buf;
+}
+
+
+
+char **get_note_from_file(char *path, unsigned int *size) {
+    FILE *f = fopen(path, "r");
+
+    if (f == NULL) {
+        f = fopen(path, "w");
+        return NULL;
+    }
+    int len = 100;
+    char **content = reset_size(content, len);
+    
+    int i = 0;
+    while(!feof(f)) {
+        char *buf = malloc(sizeof(char) * STR_L);
+        fgets(buf, STR_L, f);
+
+        content[i] = malloc(sizeof(char) * strlen(buf));
+        strcpy(content[i++], buf);
+        free(buf);
+
+        if (i == len) {
+            len <<= 1;
+            content = reset_size(content, len);
+        }
+    }
+
+    content = reset_size(content, i);
+
+    return content;
+}
